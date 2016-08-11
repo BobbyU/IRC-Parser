64cec34244e6d7e894af7cc64f281b79a2cf97d1
Robert M. Unverzagt <528872@ad.seagate.com>
2016-08-11 11:18:50 -0600

    add some comments

--- logParser.py
+++ logParser.py
@@ -3,30 +3,31 @@ import os
 import operator
 
 def main():
-	infile = open( os.path.join( os.path.abspath(__file__ + "/../../"), 'GeekShed', '#dtdgb.log'), 'r')
-	nameFreq = {}
-	nameCount = {}
+	infile = open( os.path.join( os.path.abspath(__file__ + "/../../"), 'GeekShed', '#dtdgb.log'), 'r')    #find log file
+	nameFreq = {}    #dict with names and number of talks
+	nameCount = {}    #dict with names and percent of talks
 	totalTalks = 0
 	throbCount = 0
-	for line in infile:
-		res = re.search("<.*?>", line)
-		if res:
-			name = res.group(0)[1:-1]
-			if name in nameFreq:
-				nameFreq[name] += 1
+	for line in infile:    #for each line in the log
+		res = re.search("<.*?>", line)   #use regex to find the speaker
+		if res:                          #if there is one...
+			name = res.group(0)[1:-1]    #remove the "brackets" from the string
+			if name in nameFreq:         #if the name is already in the dict
+				nameFreq[name] += 1      #then add one to their 
 				totalTalks += 1
-			else:
-				nameFreq[name] = 1
+			else:                        #if not,
+				nameFreq[name] = 1       #init their place in the dict
+				totalTalks += 1
 		
-		if re.search('throb', line):
+		if re.search('throb', line):     #count those throbbers
 			throbCount += 1
 		
-	print("Throb Count: {}\n".format(throbCount) )
+	print("Throb Count: {}\n".format(throbCount) )    # print throb count
 	
-	for key in nameFreq:
+	for key in nameFreq:    #calculate percentages
 		nameCount[key] = float(nameFreq[key]) / totalTalks
-	for key in nameFreq:
-		if nameCount[key] > 0.01:
+	for key in nameFreq:    #print each name and values
+		if nameCount[key] > 0.01:    #if they've talked enough
 			print("{}: {} ({:.2%})".format(key.ljust(20), nameFreq[key], nameCount[key]) )
 	
 	print("total talks: {}".format(totalTalks) )
