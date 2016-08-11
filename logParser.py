edfcf68b758e2ea2e3e80431d7407f4b1a9d270f
Robert M. Unverzagt <528872@ad.seagate.com>
2016-08-11 08:53:50 -0600

    Initial Commit

---
+++ logParser.py
@@ -0,0 +1,32 @@
+import re
+import operator
+
+def main():
+	infile = open('#dtdgb.log', 'r')
+	nameFreq = {}
+	nameCount = {}
+	totalTalks = 0
+	throbCount = 0
+	for line in infile:
+		res = re.search("<.*?>", line)
+		if res:
+			name = res.group(0)[1:-1]
+			if name in nameFreq:
+				nameFreq[name] += 1
+				totalTalks += 1
+			else:
+				nameFreq[name] = 1
+		
+		if re.search('throb', line):
+			throbCount += 1
+		
+	print("Throb Count: {}\n".format(throbCount) )
+	
+	for key in nameFreq:
+		nameCount[key] = float(nameFreq[key]) / totalTalks
+	for key in nameFreq:
+		print("{}: {} ({:.2%})".format(key.ljust(20), nameFreq[key], nameCount[key]) )
+	
+	print("total talks: {}".format(totalTalks) )
+
+main()
