29a7c1a5e63996ccfa9aa2c89f300e4550cea868
Bobby Unverzagt <robert.unverzagt@gmail.com>
2016-08-30 12:41:18 -0600

    added a new script which only tracks the last hour

---
+++ recent_logParser.py
@@ -0,0 +1,42 @@
+import re
+import os
+from datetime import datetime, timedelta
+
+def main():
+	infile = open( os.path.join( os.path.abspath(__file__ + "/../../"), 'GeekShed', '#dtdgb.log'), 'r')    #find log file
+	nameFreq = {}    #dict with names and number of talks
+	nameCount = {}    #dict with names and percent of talks
+	totalTalks = 0
+	throbCount = 0
+	currentTime = datetime.today() - timedelta(hours = 1);
+	for line in infile:    #for each line in the log
+		if line.__len__() > 3:
+			if line[3] == " ":
+				timestmp = datetime.strptime(line[0:15], "%b %d %H:%M:%S") + timedelta(days = 42369)
+				if currentTime < timestmp:
+					res = re.search("<.*?>", line)   #use regex to find the speaker
+					if res:                          #if there is one...
+						name = res.group(0)[1:-1].lower()    #remove the "brackets" from the string
+						if name in nameFreq:         #if the name is already in the dict
+							nameFreq[name] += 1      #then add one to their 
+							totalTalks += 1
+						else:                        #if not,
+							nameFreq[name] = 1       #init their place in the dict
+							totalTalks += 1
+					
+					if re.search('throb', line, flags=re.IGNORECASE):     #count those throbbers
+						throbCount += 1
+	
+	for key in nameFreq:    #calculate percentages
+		nameCount[key] = float(nameFreq[key]) / totalTalks
+	for i in range(0,len(nameFreq)):    #print each name and values
+		maxKey = max(nameFreq, key=nameFreq.get)    #get the entry with the most talks
+		if nameCount[maxKey] > 0.01:    #if they've talked enough
+			print("{}: {} ({:.2%})".format(maxKey.ljust(20), nameFreq[maxKey], nameCount[maxKey]) )
+			del nameCount[maxKey]
+			del nameFreq[maxKey]
+	
+	print("total talks: {}".format(totalTalks) )      # print total talks
+	print("Throb Count: {}\n".format(throbCount) )    # print throb count
+
+main()
