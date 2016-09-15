d78e17fd341e3bfba208bcbd253c9f58275155b5
Bobby Unverzagt <robert.unverzagt@gmail.com>
2016-09-15 09:52:53 -0600

    count both throbs AND roosts

--- logParser.py
+++ logParser.py
@@ -10,7 +10,9 @@ def main():
 	totalTalks = 0
 	totalWords = 0
 	throbCount = 0
+	roostCount = 0
 	throbCounter = {}
+	roostCounter = {}
 	#bPipe = 0
 	for line in infile:    #for each line in the log
 		res = re.search("<.*?>", line)   #use regex to find the speaker
@@ -21,18 +23,26 @@ def main():
 			else:                        #if not,
 				nameFreq[name] = 1       #init their place in the dict
 				throbCounter[name] = 0
+				roostCounter[name] = 0
 				wordCount[name] = 0
 			totalTalks += 1
 			totalWords += line.split().__len__()
 			wordCount[name] += line.split().__len__()
 			
-			throbsInThisLine = line.lower().count('roost') #count those throbbers
+			throbsInThisLine = line.lower().count('throb') #count those throbbers
 			throbCount += throbsInThisLine
-			if name.lower() == 'rooster':
+			if name.lower().count('throb') > 0:
 				throbCounter[name] += (throbsInThisLine - 1)
 				throbCount -= 1
 			else:
 				throbCounter[name] += throbsInThisLine
+			roostsInThisLine = line.lower().count('roost') #count those roosts
+			roostCount += roostsInThisLine
+			if name.lower() == 'rooster':
+				roostCounter[name] += (roostsInThisLine - 1)
+				roostCount -= 1
+			else:
+				roostCounter[name] += roostsInThisLine
 		#elif line[13] == "*":
 		#	if line.lower().count('broken pipe') > 0:
 		#		bPipe += 1
@@ -40,17 +50,18 @@ def main():
 	for key in nameFreq:    #calculate percentages
 		nameCount[key] = float(nameFreq[key]) / totalTalks
 		
-	print("{} {} {}  Roosts".format("Name".ljust(20), "Talks".ljust(13), "Avg Words"))
+	print("{} {} {}  Roosts  Throbs".format("Name".ljust(20), "Talks".ljust(13), "Avg Words"))
 	for i in range(0,len(nameFreq)):    #print each name and values
 		maxKey = max(nameFreq, key=nameFreq.get)    #get the entry with the most talks
 		if nameCount[maxKey] > 0.01:    #if they've talked enough
-			print("{} {:4} ({:2.2%}) {:9} {:3}".format(maxKey.ljust(20), nameFreq[maxKey], nameCount[maxKey], (wordCount[maxKey]/nameFreq[maxKey]), throbCounter[maxKey]) )
+			print("{} {:4} ({:2.2%}) {:9} {:7} {:7}".format(maxKey.ljust(20), nameFreq[maxKey], nameCount[maxKey], (wordCount[maxKey]/nameFreq[maxKey]), roostCounter[maxKey], throbCounter[maxKey]) )
 			del nameCount[maxKey]
 			del nameFreq[maxKey]
 			del throbCounter[maxKey]
 	
 	print("total talks: {}".format(totalTalks) )      # print total talks
-	print("Roost Count: {}".format(throbCount) )    # print throb count
+	print("Roost Count: {}".format(roostCount) )    # print roost count
+	print("Throb Count: {}".format(throbCount) )    # print throb count
 	print("Average words per talk: {}\n".format(totalWords/totalTalks) )    # print average words per talk
 
 main()
